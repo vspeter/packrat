ef498fe988c1a336f8d63b0e6320242df0af7e6c
Peter Howe <peter.howe@emc.com>
2015-08-12 16:32:44 -0600

    ignoring settings.py

--- packrat/settings.py
+++ packrat/settings.py
@@ -2,11 +2,22 @@
 #  settings.py
 #
 # This is a place holder that will get replaced during package install with a 
-# symlink to /etc/packrat/master.conf
+# symlink to /etc/packrat/master.conf.  Any Content in this file is lost when the
+# package is installed.
 #
 # for development work, replace this file with a symlink to your master.conf file
 # which should be a derived from master.conf.sample.  In theory master.conf.sample
 # should not have to be adjusted to much to fit your development environment
 #
-# this file is in the .gitignore to help your local symlink from getting commited
+# if you do change the file (ie create a symlink from it) run:
+#
+# git update-index --assume-unchanged packrat/settings.py
+#
+# to keep git from sending up stream your changes.
+# if you really do want to change this file, run:
+#
+# git update-index --no-assume-unchanged packrat/settings.py
+#
+# and then git will track changes to this file again
+#
 #
